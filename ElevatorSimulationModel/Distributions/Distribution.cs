28f017136ed92029ccc162371db098ddf9649cd9
Artemiy <artemiy.kashintsev@gmail.com>
2017-12-15 14:21:14 +0300

    Add distributions hierarchy

---
+++ ElevatorSimulationModel/Distributions/Distribution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulationModel.Distributions
+{
+    abstract class Distribution
+    {
+        public abstract int GetTime();
+    }
+}
---
+++ ElevatorSimulationModel/Distributions/UniformDistribution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulationModel.Distributions
+{
+    class UniformDistribution : Distribution
+    {
+        public UniformDistribution(int leftBorder, int rightBorder)
+        {
+            this.leftBorder = leftBorder;
+            this.rightBorder = rightBorder;
+
+            CheckBorders();
+        }
+        public override int GetTime()
+        {
+            return leftBorder + randGenerator.Next(rightBorder - leftBorder);
+        }
+
+        private int leftBorder;
+        private int rightBorder;
+        private Random randGenerator = new Random();
+
+        private void CheckBorders()
+        {
+            if (leftBorder > rightBorder)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
