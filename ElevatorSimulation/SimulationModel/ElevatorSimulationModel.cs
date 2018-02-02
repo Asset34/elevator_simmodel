60d19bee90562331c3dc6e3333f9df25cbaa083a
Asset34 <artemiy.kashintsev@gmail.com>
2018-02-02 17:52:34 +0300

    Implemented the creation of simulation model

--- ElevatorSimulation/SimulationModel/ElevatorSimulationModel.cs
+++ ElevatorSimulation/SimulationModel/ElevatorSimulationModel.cs
@@ -10,8 +10,10 @@ using ElevatorSimulation.SimulationModel.Controllers;
 using ElevatorSimulation.SimulationModel.Transactions;
 using ElevatorSimulation.SimulationModel.Distributions;
 using ElevatorSimulation.SimulationModel.Entities;
+using ElevatorSimulation.SimulationModel.Schedulers.CallSchedulers;
+using ElevatorSimulation.SimulationModel.Schedulers.GlobalSchedulers;
 
-namespace ElevatorSimulation
+namespace ElevatorSimulation.SimulationModel
 {
     /// <summary>
     /// Simulation model of the elevators system
@@ -37,7 +39,28 @@ namespace ElevatorSimulation
 
         public ElevatorSimulationModel(SimulationParameters parameters)
         {
-                               
+            // Build group controllers
+            TenantGeneratorsController generatorsController = BuildGeneratorsController(parameters.NumFloors);
+            TenantQueuesController queuesController = BuildQueuesController(parameters.NumFloors);
+            ElevatorsController elevatorsController = BuildElevatorsController(
+                parameters.NumFloors,
+                parameters.NumElevators,
+                parameters.ElevatorParameters);
+
+            // Build distributions
+            Distribution[] generationDistributions
+                = BuildGenerationDistributions(parameters.NumFloors, parameters.TenantGeneratorParameters);
+            Distribution[] movementDistributions
+                = BuildMovementDistributions(parameters.NumElevators, parameters.ElevatorParameters);
+
+            // Build event controller
+            m_eventController = new EventController(
+                generatorsController,
+                queuesController,
+                elevatorsController,
+                generationDistributions,
+                movementDistributions
+                );
         }
         /// <summary>
         /// Run the model
@@ -54,18 +77,89 @@ namespace ElevatorSimulation
             // Reset events
             m_events.Clear();
 
-            // Reset controllers
-            m_floorQueueController.Reset();
-            m_elevatorController.Reset();
         }
 
+        private TenantGeneratorsController BuildGeneratorsController(int numFloors)
+        {
+            TenantGeneratorsController controller = new TenantGeneratorsController();
+            Distribution floorsDistribution = new UniformDistribution(1, numFloors);
+            for (int i = 0; i < numFloors; i++)
+            {
+                controller.Generators.Add(new TenantGenerator(i + 1, floorsDistribution));
+            }
+
+            return controller;
+        }
+        private TenantQueuesController BuildQueuesController(int numFloors)
+        {
+            TenantQueuesController controller = new TenantQueuesController();
+            for (int i = 0; i < numFloors; i++)
+            {
+                controller.Queues.Add(new TenantQueue(i + 1));
+            }
+
+            return controller;
+        }
+        private ElevatorsController BuildElevatorsController(int numFloors, int numElevators, ElevatorParameters[] parameters)
+        {
+            // Build global scheduler
+            GlobalScheduler scheduler = new NearestCarScheduler(numFloors);
+
+            // Build controller
+            ElevatorsController controller = new ElevatorsController(scheduler);
+            Elevator elevator;
+            for (int i = 0; i < numElevators; i++)
+            {
+                elevator = BuildElevator(i + 1, parameters[i]);
+                controller.Elevators.Add(elevator);
+            }
+
+            return controller;
+        }
+        private Elevator BuildElevator(int id, ElevatorParameters parameters)
+        {
+            // Build call scheduler
+            CallScheduler scheduler = new CollectiveCallScheduler();
+
+            // Build elevator
+            return new Elevator(
+                id,
+                parameters.Capacity,
+                parameters.StartFloor,
+                scheduler);
+        }
+        private Distribution[] BuildGenerationDistributions(int numFloors, TenantGeneratorParameters[] parameters)
+        {
+            Distribution[] distributions = new Distribution[numFloors];
+            int min, max;
+            for (int i = 0; i < numFloors; i++)
+            {
+                min = parameters[i].Period - parameters[i].DPeriod;
+                max = parameters[i].Period + parameters[i].DPeriod;
+
+                distributions[i] = new UniformDistribution(min, max);
+            }
+
+            return distributions;
+        }
+        private Distribution[] BuildMovementDistributions(int numElevators, ElevatorParameters[] parameters)
+        {
+            Distribution[] distributions = new Distribution[numElevators];
+            int min, max;
+            for (int i = 0; i < numElevators; i++)
+            {
+                min = parameters[i].Period - parameters[i].DPeriod;
+                max = parameters[i].Period + parameters[i].DPeriod;
+
+                distributions[i] = new UniformDistribution(min, max);
+            }
+
+            return distributions;
+        }
+
+
         private List<Event> m_events = new List<Event>();
-        /* Distributions */
-        private readonly Distribution[] m_generationDistributions;
-        private readonly Distribution[] m_movementDistributions;
-        /* Controllers */
-        private readonly GroupTenantGeneratorController m_tenantGeneratorController;
-        private readonly GroupFloorQueueController m_floorQueueController;
-        private readonly GroupElevatorController m_elevatorController;
+
+        private readonly EventController m_eventController;
     }
 }
