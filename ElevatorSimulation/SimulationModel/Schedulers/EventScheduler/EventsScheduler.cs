69570c27206a70c3230447c81b8d7fac391b240b
Artemiy <artemiy.kashintsev@gmail.com>
2018-02-13 09:54:12 +0300

    Implement priority queue of events and add it to the scheduler

---
+++ ElevatorSimulation/SimulationModel/Schedulers/EventScheduler/EventsScheduler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using ElevatorSimulation.SimulationModel.Events;
+
+namespace ElevatorSimulation.SimulationModel.Schedulers
+{
+    /// <summary>
+    /// Schedulers of simulatuon model's events
+    /// </summary>
+    partial class EventsScheduler
+    {
+        public bool IsEmpty
+        {
+            get { return m_events.Count == 0; }
+        }
+
+        public void Add(Event ev)
+        {
+            if (!m_events.ContainsKey(ev.Time))
+            {
+                m_events.Add(ev.Time, new PriorityEventQueue());
+            }
+
+            m_events[ev.Time].Enqueue(ev);
+        }
+        public Event Remove(int time)
+        {
+            Event ev = m_events[time].Dequeue();
+
+            if (m_events[time].Count == 0)
+            {
+                m_events.Remove(time);
+            }
+
+            return ev;
+        }
+        public Event Schedule()
+        {
+            // Check
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("No events to schedule");
+            }
+
+            return Remove(m_events.Keys.Min());
+        }
+
+        public void Reset()
+        {
+            m_events.Clear();
+        }
+
+        private Dictionary<int, PriorityEventQueue> m_events = new Dictionary<int, PriorityEventQueue>();
+    }
+}
---
+++ ElevatorSimulation/SimulationModel/Schedulers/EventScheduler/PriorityEventQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ElevatorSimulation.SimulationModel.Events;
+
+namespace ElevatorSimulation.SimulationModel.Schedulers
+{
+    partial class EventsScheduler
+    {
+        class PriorityEventQueue
+        {
+            public int Count
+            {
+                get { return m_events.Count; }
+            }
+
+            public void Enqueue(Event ev)
+            {
+                m_events.Add(ev);
+            }
+            public Event Dequeue()
+            {
+                Event ev = Peek();
+                m_events.Remove(ev);
+
+                return ev;
+            }
+            public Event Peek()
+            {
+                return m_events.OrderByDescending(ev => ev.Priority).First();
+            }
+            public void Clear()
+            {
+                m_events.Clear();
+            }
+
+            private List<Event> m_events = new List<Event>();
+        }
+    }
+}
